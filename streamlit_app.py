d245f66c716b4bccd6556d81d9dcb93b3b68e586
ogr0818 <sammy336588@gmail.com>
2026-01-13 16:05:58 +0800

    add new tabel

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,35 @@ st.title("資料視覺化")
 st.write('''
             # 說明：
             1. 目的：視覺化盤點量與電腦庫存量間的差異
-            2. 選擇欄位-盤點量：年終點量
-            3. 選擇欄位-盤點量：盤點量扣除電腦庫存差
+            2. 選擇欄位-盤點量：12/30 盤點量
+            3. 選擇欄位-盤點量與電腦庫存差：盤點量扣除電腦庫存差
             4. 輸出二種**圖表**供分析
+            5. 表格： 結果呈現
+         - 長條圖：實際量與電腦量比較
+         - 矩形樹狀圖： 實際量與電腦量差的比較
             ''')
 file_path = "./static/data_2026.xlsx"
 data = pd.read_excel(file_path)
 st.divider()
-col = ['藥品代碼', '藥名', r'12/27盤點量', r'12/30預期量', '備註']
+col = ['藥品代碼', '藥名', r'12/30預期量', r'12/30電腦量', '備註']
 df = data[col]
 # conditions
 limit = st.radio("盤點量：", ["盤點量非0", "盤點量為0"], index=0, horizontal=True)
 type = st.radio("劑型：", ["口服", "針劑", "其他"], index=0, horizontal=True)
-note = st.radio("年終盤點量與電腦庫存差：", ["錯誤", "無資料", "OK"], index=0, horizontal=True)
-if limit == "盤點量非0":
-    result = df.query(f"`{'12/27盤點量'}` != 0")
-else:
-    result = df.query(f"`{'12/27盤點量'}` == 0")
-    pass
+note = st.radio("盤點量與電腦庫存差：", ["錯誤", "無資料", "OK"], index=0, horizontal=True)
 
-df['type'] = df['藥品代碼'].str[-1]
-if type == "口服":
-    resultX = df.query("type == 'O'")
-elif type == "針劑":
-    resultX = df.query("type == 'I'")
+if limit == "盤點量非0":
+    result = df.query(f"`{'12/30預期量'}` != 0")
 else:
-    resultX = df.query("type != 'I' and type != 'O'")
+    result = df.query(f"`{'12/30預期量'}` == 0")
+
+result['type'] = result['藥品代碼'].str[-1]
+if type == "口服":
+    resultX = result.query("type == 'O'")
+elif type == "針劑":
+    resultX = result.query("type == 'I'")
+else:
+    resultX = result.query("type != 'I' and type != 'O'")
 
 if note == "無資料":
     resultZ = resultX.query("備註 == @note")
@@ -42,20 +45,38 @@ elif note == "錯誤":
 else:
     resultZ = resultX.query("備註 == 'OK'")
 # st.write(resultZ.head(10))
-st.markdown(f"### 分析總筆數： {resultZ.shape[0]}")
+resultZ.rename(columns={'12/30預期量':'12/30量'}, inplace=True)
+resultZ.drop(columns=['type'], inplace=True)
+st.markdown(f"### 分析總筆數： 共 **{resultZ.shape[0]}** 筆")
+
+st.divider()
+st.title("表格：")
+st.subheader("依藥品代碼排序")
+st.write(resultZ)
+
 st.divider()
 st.title("圖表：")
-fig_type = st.radio("選擇圖表： ", ['長條圖', '矩形樹狀圖'], index=0, horizontal=True)
-fig_data = resultZ[['藥名', r'12/27盤點量', r'12/30預期量']]
-fig_data = fig_data.sort_values(by='12/27盤點量', ascending=False)
-if fig_type == '長條圖':
-    fig = px.bar(fig_data, x='藥名', y=[r'12/27盤點量', r'12/30預期量'])
-    fig.update_layout(
-            xaxis_tickangle=-45
-        )   
-    fig.update_layout(yaxis_title="數量")
-    st.plotly_chart(fig, use_container_width=True)
+fig_type = st.radio("選擇圖表： ", [r'長條圖(依12/30盤點量)', '矩形樹狀圖'], index=0, horizontal=True)
+fig_data = resultZ[['藥名', r'12/30量', r'12/30電腦量', '備註']]
+fig_data = fig_data.sort_values(by='12/30量', ascending=False)
+if note != '無資料':
+    if fig_data.empty:
+        st.markdown("### 無資料")
+    else:
+        if fig_type == '長條圖':
+            fig = px.bar(fig_data, x='藥名', y=[r'12/30量', r'12/30電腦量'])
+            fig.update_layout(
+                    xaxis_tickangle=-45
+                )   
+            fig.update_layout(yaxis_title="數量")
+            st.plotly_chart(fig, use_container_width=True)
+        else:
+            fig_data['diff'] = fig_data[r'12/30量'] - fig_data[r'12/30電腦量']
+            if 0 not in fig_data['diff'].unique():
+                fig_map = px.treemap(fig_data, path=['藥名'], values=fig_data['diff'],
+                                    color='12/30量', color_continuous_scale='Blues')
+                st.plotly_chart(fig_map, use_container_width=True)
+            else:
+                st.markdown("### 無資料")
 else:
-    diff = fig_data['12/27盤點量'] - fig_data['12/30預期量']
-    fig_map = px.treemap(fig_data, path=['藥名'], values=diff, color='12/27盤點量')
-    st.plotly_chart(fig_map, use_container_width=True)
+    st.title(note)
